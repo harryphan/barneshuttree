e58bca7f24071e0292df7b2764d904086e5fcf27
harryphan <harryphan@gmail.com>
2014-08-13 23:16:54 -0400

    added test function

--- js/bhtree.js
+++ js/bhtree.js
@@ -1,25 +1,92 @@
 define([],function () {
 	var x;
    
-	var min={x:0,y:0,z:0};
-	var max={x:8,y:8,z:8};
-	init(min,max);
+	var testMin={x:0,y:0,z:0};
+	var testMax={x:8,y:8,z:8};
+	var bhRoot;
+	test();
+	function test(){
+		init(testMin,testMax);
+		var testPoint={x:7,y:7,z:7};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==0);
+		var testPoint={x:3,y:7,z:7};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==1);
+		var testPoint={x:3,y:3,z:7};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==2);
+		var testPoint={x:7,y:3,z:7};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==3);
+		var testPoint={x:7,y:7,z:3};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==4);
+		var testPoint={x:3,y:7,z:3};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==5);
+		var testPoint={x:3,y:3,z:3};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==6);
+		var testPoint={x:7,y:3,z:3};
+		var answer=getQuadrant(testPoint,bhRoot.box);
+		console.log(answer==7);
+	}
 	function TreeNode(min,max){
 		return{
-			body:[],
+			point:[],
 			leaf:true,
 			children:[null,null,null,null,null,null,null,null],
-			box:[min,max]
+			box:{min:min,max:max}
 		}
 	}
 	function init(min,max){
-		bhRoot=new Treenode(min,max);
+		bhRoot=new TreeNode(min,max);
 	}
 	function add(point){
 		addNode(bhRoot,point);
 	}
 	function addNode(node, point){
-		if(node.leaf){
+		if(node.point.length >0){
+			console.log(true)
+		}else{
+			var quadrant=getQuadrant(point,node.box);
+		}
+	}
+	function getQuadrant(point,box){
+		var center = {x:(box.min.x + box.max.x)/2.0,y:(box.min.y + box.max.y)/2.0,z:(box.min.z + box.max.z)/2.0}
+		if (point.z > center.z ){
+			if(point.y>center.y){
+				if(point.x > center.x){
+					return 0
+				}
+				else{
+					return 1
+				}
+			}else{
+				if(point.x > center.x){
+					return 3
+				}
+				else{
+					return 2
+				}
+			}
+		}else{
+			if(point.y>center.y){
+				if(point.x > center.x){
+					return 4
+				}
+				else{
+					return 5
+				}
+			}else{
+				if(point.x > center.x){
+					return 7
+				}
+				else{
+					return 6
+				}
+			}
 		}
 	}
 });
