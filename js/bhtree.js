f43dbfb68505fe78760aa9fbafd491936b7463e5
harryphan <harryphan@gmail.com>
2014-08-14 01:17:08 -0400

    tree creation

--- js/bhtree.js
+++ js/bhtree.js
@@ -4,7 +4,6 @@ define([],function () {
 	var testMin={x:0,y:0,z:0};
 	var testMax={x:8,y:8,z:8};
 	var bhRoot;
-	test();
 	function test(){
 		init(testMin,testMax);
 		var testPoint={x:7,y:7,z:7};
@@ -32,61 +31,144 @@ define([],function () {
 		var answer=getQuadrant(testPoint,bhRoot.box);
 		console.log(answer==7);
 	}
+	function test2(){
+		init(testMin,testMax);
+		var testPoint={x:7,y:7,z:7};
+		console.log(createChild(testPoint,0,bhRoot.box));
+	}
+	function test3(){
+		var t = TreeNode(testMin,testMax);
+		var testPoint={x:7,y:7,z:7};
+		t.addChild(testPoint);
+		var testPoint={x:1,y:1,z:1};
+		t.addChild(testPoint);
+		var testPoint={x:5,y:5,z:5};
+		t.addChild(testPoint);
+		console.log(t);
+	}
+	function test4(){
+		init(testMin,testMax);
+		var testPoint={x:7,y:7,z:7};
+		add(testPoint);
+		var testPoint={x:1,y:1,z:1};
+		add(testPoint);
+		var testPoint={x:5,y:5,z:5};
+		add(testPoint);
+		console.log(t);
+	}
 	function TreeNode(min,max){
+		var children = [null,null,null,null,null,null,null,null];
+		var leaf = true;
+		var box={min:min,max:max};
+		var point={};
+		function addChild(point){
+			var quadrant=getQuadrant(point,box);
+			var child=children[quadrant];
+			if(child){
+				if(child.leaf){
+					var node=new TreeNode(child.box.min,child.box.max);
+					node.leaf=false;
+					var points=[point,child.point];
+					node.addChild(point);
+					node.addChild(child.point);
+					children[quadrant]=node;
+				}else{
+					child.addChild(child.point);
+				}
+				
+			}else{
+				children[quadrant]=createChild(point, quadrant,box);
+				leaf=false;
+			}
+		}
+		function createChild(point,quadrant,bbox){
+			var min = {
+				x:(bbox.min.x + bbox.max.x)/2.0,
+				y:(bbox.min.y + bbox.max.y)/2.0,
+				z:(bbox.min.z + bbox.max.z)/2.0
+			};
+			switch(quadrant){
+				case 0:
+					max=bbox.max;
+					break;
+				case 1:
+					max={x:bbox.min.x,y:bbox.max.y,z:bbox.max.z}
+					break;
+				case 2:
+					max={x:bbox.min.x,y:bbox.min.y,z:bbox.max.z}
+					break;
+				case 3:
+					max={x:bbox.max.x,y:bbox.min.y,z:bbox.max.z}
+					break;
+				case 4:
+					max={x:bbox.max.x,y:bbox.max.y,z:bbox.min.z}
+					break;
+				case 5:
+					max={x:bbox.min.x,y:bbox.max.y,z:bbox.min.z}
+					break;
+				case 6:
+					max=bbox.min
+					break;
+				default:
+					max={x:bbox.max.x,y:bbox.min.y,z:bbox.min.z}
+					break;
+			}
+			var child=new TreeNode(min,max);
+			child.point=point;
+			return child;
+		}
+		function getQuadrant(point,box){
+			var center = {x:(box.min.x + box.max.x)/2.0,y:(box.min.y + box.max.y)/2.0,z:(box.min.z + box.max.z)/2.0};
+			if (point.z > center.z ){
+				if(point.y>center.y){
+					if(point.x > center.x){
+						return 0
+					}
+					else{
+						return 1
+					}
+				}else{
+					if(point.x > center.x){
+						return 3
+					}
+					else{
+						return 2
+					}
+				}
+			}else{
+				if(point.y>center.y){
+					if(point.x > center.x){
+						return 4
+					}
+					else{
+						return 5
+					}
+				}else{
+					if(point.x > center.x){
+						return 7
+					}
+					else{
+						return 6
+					}
+				}
+			}
+		}
 		return{
-			point:[],
-			leaf:true,
-			children:[null,null,null,null,null,null,null,null],
-			box:{min:min,max:max}
+			point:point,
+			leaf:leaf,
+			addChild:addChild,
+			box:box,
+			children:children
 		}
 	}
 	function init(min,max){
 		bhRoot=new TreeNode(min,max);
 	}
 	function add(point){
-		addNode(bhRoot,point);
+		bhRoot.addChild(point);
 	}
-	function addNode(node, point){
-		if(node.point.length >0){
-			console.log(true)
-		}else{
-			var quadrant=getQuadrant(point,node.box);
-		}
-	}
-	function getQuadrant(point,box){
-		var center = {x:(box.min.x + box.max.x)/2.0,y:(box.min.y + box.max.y)/2.0,z:(box.min.z + box.max.z)/2.0}
-		if (point.z > center.z ){
-			if(point.y>center.y){
-				if(point.x > center.x){
-					return 0
-				}
-				else{
-					return 1
-				}
-			}else{
-				if(point.x > center.x){
-					return 3
-				}
-				else{
-					return 2
-				}
-			}
-		}else{
-			if(point.y>center.y){
-				if(point.x > center.x){
-					return 4
-				}
-				else{
-					return 5
-				}
-			}else{
-				if(point.x > center.x){
-					return 7
-				}
-				else{
-					return 6
-				}
-			}
-		}
+	return{
+		init:init,
+		add:add
 	}
 });
